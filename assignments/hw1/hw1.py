3dd8320913436358cf53cdee6170ce5919dd0bfc
RoyalBlueSix <faustsw@g.cofc.edu>
2022-01-19 11:09:45 -0500

    First 3 codes of HW1.

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -12,15 +12,26 @@ I certify that this assignment is my own work, but I discussed it with: <Name(s)
 
 
 def calc_rec_area():
-    pass
+    W = eval(input("Enter rectangle width: "))
+    L = eval(input("Enter rectangle length: "))
+    area = W * L
+    print(area)
 
 
 def calc_volume():
-    pass
+    H = eval(input("Enter rectangle height: "))
+    W = eval(input("Enter rectangle width: "))
+    L = eval(input("Enter rectangle length: "))
+    V = W * L * H
+    print(V)
+
 
 
 def shooting_percentage():
-    pass
+    Total_shots = eval(input('Enter how many shots the player took: '))
+    Made = eval(input('Enter how many shots the player made: '))
+    percentage = (Made / Total_shots) * 100
+    print('The player made', percentage, '% of their shots.')
 
 
 def coffee():
