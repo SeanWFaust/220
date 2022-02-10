9f29a080cb75e3b4d9ffdcee2892df0a860f31b4
RoyalBlueSix <faustsw@g.cofc.edu>
2022-02-10 10:41:36 -0500

    HW4 Submission

--- assignments/hw4/hw4.py
+++ assignments/hw4/hw4.py
@@ -46,8 +46,29 @@ def squares():
 
 
 def rectangle():
-    pass
-
+    width = 400
+    height = 400
+    win = GraphWin("Rectangle", width, height)
+    click1 = win.getMouse()
+    click2 = win.getMouse()
+    rec = Rectangle(Point(click1.getX(), click1.getY()), Point(click2.getX(), click2.getY()))
+    rec.setOutline("Green")
+    rec.setFill("Green")
+    rec.draw(win)
+    length = (400 - click1.getX()) - (400 - click2.getX())
+    hei = (400 - click1.getY()) - (400 - click2.getY())
+    perimeter = (length * 2) + (hei * 2)
+    perimeter_pt = Point(200, 20)
+    perimeter_text = Text(perimeter_pt, "Perimeter : ")
+    perimeter_text.draw(win)
+    perm_num_pt = Point(250, 20)
+    perm_num = Text(perm_num_pt, perimeter)
+    perm_num.draw(win)
+    close_pt = Point(200, 200)
+    close_text = Text(close_pt, "Click again to close")
+    close_text.draw(win)
+    win.getMouse()
+    win.close()
 
 def circle():
     pass
