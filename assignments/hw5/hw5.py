3f8e1321ce77acfda2ad5df1012288ad567a53a7
RoyalBlueSix <faustsw@g.cofc.edu>
2022-02-14 12:53:45 -0500

    HW5 Submission

--- assignments/hw5/hw5.py
+++ assignments/hw5/hw5.py
@@ -1,42 +1,76 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Sean Faust
+HW5.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: This homework focuses on the different string methods
+within python.
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
 def name_reverse():
-    pass
+    name = input("Enter your first then last name:")
+    spname = name.split()
+    print(spname[1] + ', ' + spname[0])
 
 
 def company_name():
-    pass
+    url = input("Enter a companies URL:")
+    comp_name = url.split('.')
+    print(comp_name[1])
 
 
 def initials():
-    pass
+    num_stud = eval(input("How many students are in the class?"))
+    for i in range(1, num_stud + 1):
+        stud_name = input("What is the name of student " + str(i) + "?")
+        listnames = stud_name.split()
+        fname = listnames[0]
+        lname = listnames[1]
+        print(fname[0] + lname[0])
 
 
 def names():
-    pass
+    enterednames = input("Enter a list of names:")
+    listnames = enterednames.split(',')
+    for name in listnames:
+        spltname = name.split()
+        fname = spltname[0]
+        lname = spltname[1]
+        print(fname[0] + lname[0], end=' ')
 
 
 def thirds():
-    pass
+    splt = []
+    num_sen = eval(input("Enter the number of sentences:"))
+    for i in range(1, num_sen + 1):
+        sentence = input("What is sentence " + str(i) + "?")
+        spltsen = sentence[0:len(sentence):3]
+        splt = splt + [spltsen]
+    print(''.join(splt))
 
 
 def word_average():
-    pass
+    totalchar = 0
+    sentence = input("Enter a sentence:")
+    lsen = sentence.split()
+    for word in lsen:
+        totalchar += len(word)
+    avg = totalchar / len(lsen)
+    print("The average word length is: " + str(avg))
 
 
 def pig_latin():
-    pass
+    piglist = []
+    sentence = input("Enter a sentence:")
+    spltsentence = sentence.split()
+    for word in spltsentence:
+        pigword = (word[1:len(word)] + word[0] + 'ay')
+        piglist.append(pigword)
+    print(' '.join(piglist).lower())
+
 
 
 if __name__ == '__main__':
