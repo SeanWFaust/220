6aae39312e864eb52ff101e9f6d54a29e97ea126
RoyalBlueSix <faustsw@g.cofc.edu>
2022-02-18 13:50:51 -0500

    HW6 Submission

--- assignments/hw6/hw6.py
+++ assignments/hw6/hw6.py
@@ -1,43 +1,82 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Sean Faust
+HW6.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: Working with strings
+Writing functions that accept arguments and return values.
+Modifying an object in a parameter
+
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
+import math
 
 def cash_converter():
-    pass
+    number = input("Enter an integer:")
+    print("That is ${:,.2f}".format(float(number)))
 
 
 def encode():
-    pass
+    message = input("Enter a message:")
+    key = input("Enter a key:")
+    output = ''
+    for letter in message:
+        output += chr(ord(letter) + int(key))
+    print(output)
 
 
 def sphere_area(radius):
-    pass
+    area = 4 * math.pi * (radius ** 2)
+    print("Surface area:", area)
+    return(area)
 
 
 def sphere_volume(radius):
-    pass
+    volume = (4 / 3) * math.pi * (float(radius) ** 3)
+    print("Volume:", float(volume))
+    return float(volume)
 
 
 def sum_n(number):
-    pass
+    total = 0
+    for i in range(number + 1):
+        total += i
+    print(int(total))
+    return(int(total))
 
 
 def sum_n_cubes(number):
-    pass
+    total = 0
+    for i in range(number + 1):
+        total += i ** 3
+    print(int(total))
+    return(int(total))
 
 
 def encode_better():
-    pass
-
+    message = input("Enter a message")
+    word = input("Enter a key")
+    messagel = []
+    wordl = []
+    wordl2 = []
+    ad_message = []
+    en_message = ''
+    for letter in word:
+        wordl += letter
+    for i in range(len(message)):
+        i = i % len(word)
+        wordl2.append((ord(wordl[i])) - 65)
+    for letter in message:
+        messagel.append(ord(letter) - 65)
+    for i in messagel:
+        for j in wordl2:
+            ad_message.append(((i + j) % 58) + 65)
+    short_ad_message = ad_message[0:len(ad_message):len(message) + 1]
+    for num in short_ad_message:
+        en_message += chr(num)
+    print(en_message)
 
 if __name__ == '__main__':
     # cash_converter()
