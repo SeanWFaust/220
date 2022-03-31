dc43abdab2ba1980e6ad0060a773b8971873492d
RoyalBlueSix <faustsw@g.cofc.edu>
2022-03-31 16:28:27 -0400

    Lab10

--- labs/lab10/lab10.py
+++ labs/lab10/lab10.py
@@ -12,27 +12,21 @@ from button import Button
 from door import Door
 from graphics import *
 
+
 def main():
     width = 400
     height = 400
     win = GraphWin('button game', width, height)
     exit = Button(Rectangle(Point(100, 100), Point(300, 150)), 'Exit')
     exit.draw(win)
-    door = Door(Rectangle(Point(100, 200), Point(300, 475)), 'Closed')
+    door = Door(Rectangle(Point(100, 200), Point(300, 375)), 'Closed')
     door.draw(win)
-    i = 1
-    while i < 2:
-        i = 1
-        while i == 1:
-            if door.is_clicked(win.getMouse()):
-                door.open('red', 'Open')
-                i = 0
-            elif exit.is_clicked(win.getMouse()):
-                i = 3
-        while i == 0:
-            if door.is_clicked(win.getMouse()):
-                door.close('white', 'closed')
-                i = 1
-            elif exit.is_clicked(win.getMouse()):
-                i = 3
+    door.close('white', 'Closed')
+    while not door.is_secret():
+        if door.is_clicked(win.getMouse()) and door.close('white', 'Closed'):
+            door.open('red', 'Open')
+        if door.is_clicked(win.getMouse()) and door.open('red', 'Open'):
+            door.close('white', 'Closed')
+        elif exit.is_clicked(win.getMouse()):
+            door.set_secret(True)
     win.close()
